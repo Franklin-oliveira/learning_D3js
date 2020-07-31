f5af77b737daae1344e089a9e353856ff1f0d1ef
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2020-07-31 11:06:22 -0300

    Adjusted animation to update axes correctly - 3-dynamic/interval-loops.js

--- 3-dynamic/js/interval-loops.js
+++ 3-dynamic/js/interval-loops.js
@@ -1,5 +1,5 @@
 /* 
- * Bar chart example
+ * Bar chart example with animations
  * coffee revenue
  */
 
@@ -19,6 +19,44 @@ let svg = d3.select('#chart').append('svg')
 let g = svg.append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+// appending axes
+let xAxisGroup = g.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+
+let yAxisGroup = g.append('g')
+    .attr('class', 'y axis')
+
+
+// y scale (linear)
+let y = d3.scaleLinear()
+    .range([height, 0])
+
+// y scale (band)
+let x = d3.scaleBand()
+    .range([0, width])
+    .paddingInner(0.3)
+    .paddingOuter(0.3);
+
+// x label
+g.append('text')
+    .attr('class', 'xlabel')
+    .attr('x', width / 2)
+    .attr('y', height + 50)
+    .attr('font-size', '20px')
+    .attr('text-anchor', 'middle')
+    .text("Month")
+
+// y label
+g.append('text')
+    .attr('class', 'ylabel')
+    .attr('x', - (height / 2))
+    .attr('y', -60)
+    .attr('font-size', '20px')
+    .attr('text-anchor', 'middle')
+    .attr('transform', 'rotate(-90)')
+    .text('Value (thousands of US$)')
+
 d3.json('./data/coffee-revenue.json').then(function (data) {
     console.log(data)
 
@@ -26,19 +64,6 @@ d3.json('./data/coffee-revenue.json').then(function (data) {
         d.revenue = +d.revenue;
         d.profit = +d.profit;
     })
-
-    x.domain(data.map(function (d) {
-        return d.month;
-    }))
-
-    y.domain([0, d3.max(data, function (d) {
-        return d.revenue;
-    })])
-
-
-    d3.interval(function () {
-        update(data)
-    }, 1000);
 
     // // profit bars
     // let profBars = g.selectAll('bar').data(data)
@@ -56,73 +81,56 @@ d3.json('./data/coffee-revenue.json').then(function (data) {
     //     .attr('width', x.bandwidth)
     //     .attr('fill', '#996633')
 
+    d3.interval(function () {
+        update(data)
+    }, 1000)
+
+    // set viz for the first time
+    update(data)
+
 }).catch(function (error) {
-    
+    console.log(error)
 })
 
 function update(data) {
 
-    // y scale (linear)
-    let y = d3.scaleLinear()
-    .domain([0, d3.max(data, function (d) {
+    // updating domain
+    y.domain([0, d3.max(data, function (d) {
         return d.revenue;
     })])
-    .range([height, 0])
-
-    // y scale (band)
-    let x = d3.scaleBand()
-        .domain(data.map(function (d) {
-            return d.month;
-        }))
-        .range([0, width])
-        .paddingInner(0.3)
-        .paddingOuter(0.3);
+    x.domain(data.map(function (d) {
+        return d.month;
+    }))
 
     // x axis
     let xAxis = d3.axisBottom(x);
-    g.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis)
-
+    
     // y axis
-    let yAxis = d3.axisLeft(y);
-    g.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis)
-
-    // x label
-    g.append('text')
-        .attr('class', 'xlabel')
-        .attr('x', width / 2)
-        .attr('y', height + 50)
-        .attr('font-size', '20px')
-        .attr('text-anchor', 'middle')
-        .text("Month")
-
-    // y label
-    g.append('text')
-        .attr('class', 'ylabel')
-        .attr('x', - (height / 2))
-        .attr('y', -60)
-        .attr('font-size', '20px')
-        .attr('text-anchor', 'middle')
-        .attr('transform', 'rotate(-90)')
-        .text('Value (thousands of US$)')
-
-    // revenue bars
-    let revBars = g.selectAll('bar').data(data)
-    revBars.enter()
-        .append('rect')
-        .attr('x', function (d) {
-            return x(d.month);
+    let yAxis = d3.axisLeft(y)
+        .tickFormat(function (d) {
+            return '$' + d;
         })
-        .attr('y', function (d) {
-            return y(d.revenue);
-        })
-        .attr('height', function (d) {
-            return height - y(d.revenue)
-        })
-        .attr('width', x.bandwidth)
-        .attr('fill', '#d9b38c')
-};
+
+
+    // updating axes
+    xAxisGroup.call(xAxis)
+    yAxisGroup.call(yAxis)
+
+
+    // // revenue bars
+    // let revBars = g.selectAll('bar').data(data)
+    // revBars.enter()
+    //     .append('rect')
+    //     .attr('x', function (d) {
+    //         return x(d.month);
+    //     })
+    //     .attr('y', function (d) {
+    //         return y(d.revenue);
+    //     })
+    //     .attr('height', function (d) {
+    //         return height - y(d.revenue)
+    //     })
+    //     .attr('width', x.bandwidth)
+    //     .attr('fill', '#d9b38c')
+
+}
